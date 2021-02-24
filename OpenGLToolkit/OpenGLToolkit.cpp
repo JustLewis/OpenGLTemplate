22f09aabcc2fa3261e1494af6b5f501949740427
JustLewis <l.marlow@outlook.com>
2021-02-24 14:08:38 +0000

    Fixed

--- OpenGLToolkit/OpenGLToolkit.cpp
+++ OpenGLToolkit/OpenGLToolkit.cpp
@@ -18,22 +18,24 @@ Shader compShader;
 ComputeTexture Texture;
 Mesh mesh = Mesh();
 
-glm::vec2 Posa;
-glm::vec2 Posb;
-glm::vec2 Posc;
-glm::vec2 Posd;
+//Points for bezier curve
+glm::vec2 Posa; //Start
+glm::vec2 Posb; //first control point
+glm::vec2 Posc; //second control point
+glm::vec2 Posd; //ENd
 
+//texture dimensions for compute shader to render to. 
 GLuint TexWidth = 400;
 GLuint TexHeight = 400;
 
 //From Kronos - Debugging function protype. (Full function below main)
 void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam);
 
-void PlayerInput();
-float BezForShader(float t, float wa, float wb, float wc, float wd);
-void SplineOneAttempt();
-void SplineTwoAttempt();
-void Remake(float t,GLuint BufferIn);
+void PlayerInput(); //updates Posb and Posc values with controls (WASD and Arrow keys)
+float BezForShader(float t, float wa, float wb, float wc, float wd); //Bezier Algorythm for 2 control points 
+void SplineOneAttempt(); //uses compute shader
+void SplineTwoAttempt(); //uses compute shader
+void Remake(float t,GLuint BufferIn); //Remake buffer containing line (Not compute shader
 
 bool RemakeBuffer = true;
 
@@ -45,37 +47,14 @@ int main()
 	//window = Window(1920*2, 1080*2); //4k
 	window.Initialise();
 
-	//glPointSize(1);
-	//glEnable(GL_DEPTH_TEST);
+	//glEnable(GL_DEPTH_TEST); //no need for this working in 2D
 
 	//Debug stuff. Uncomment the below 2 functions for debug information
-	glDebugMessageCallback(MessageCallback, 0);
-	glEnable(GL_DEBUG_OUTPUT);
+	//glDebugMessageCallback(MessageCallback, 0);
+	//glEnable(GL_DEBUG_OUTPUT);
 		
 
-	//Polygon for texture
-	GLfloat Verts[] =
-	{
-		-1.0f,-1.0f,0.0f,	0.0f,0.0f, //Botom Left
-		1.0f,-1.0f,0.0f,	1.0f,0.0f, //Botom right
-		-1.0f,1.0f,0.0f,	0.0f,1.0f, //Top Left
-		1.0f,1.0f,0.0f,		1.0f,1.0f, //Top Left
 
-	};
-
-	GLuint Inds[] =
-	{
-		0,1,2,
-		2,1,3
-	};
-
-	//mesh.CreateMesh(Verts, Inds, 20, 6);
-	
-	
-	shader.CreateFromFiles("Shaders/Shader.vert", "Shaders/Shader.frag");
-
-	Texture.CreateTexture(TexWidth,TexHeight);
-	Texture.UseTexture();
 
 	//SplineOneAttempt();
 	//SplineTwoAttempt();
@@ -90,54 +69,28 @@ int main()
 	Posd.x = 0.8f;
 	Posd.y = -0.80f;
 
-	Points.push_back(0.0f);
-	Points.push_back(0.0f);
-	Points.push_back(0.0f);
 
 
-	if (RemakeBuffer)
-	{
-
-		for (size_t i = 0; i < NumberOfPoints; i++)
-		{
-			Points.push_back(BezForShader(t * i, Posa.x, Posb.x, Posc.x, Posd.x));
-			Points.push_back(BezForShader(t * i, Posa.y, Posb.y, Posc.y, Posd.y));
-			Points.push_back(0.0f);
-		}
-		RemakeBuffer = false;
-	}
-
-	//glEnable(GL_DEBUG_OUTPUT);
-
-	//Create buffers for lines (Make them dynamic)
+	//Create buffers for lines
 	GLuint LineBuffer,VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &LineBuffer);
 	glBindVertexArray(VAO);
+
+	Remake(t, LineBuffer); //filles buffer on cpu
+
+	//pass buffer to GPU for drawing
 	glBindBuffer(GL_ARRAY_BUFFER, LineBuffer);
 	glBufferData(GL_ARRAY_BUFFER, Points.size() * sizeof(GLfloat), Points.data(), GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0); //Changed to sizeof GLfloat because it makes more sense.
-	glEnableVertexAttribArray(0);
+	//set vertex attributes (x and y position)
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0); //Enable vertex attrivute 0
 
-	GLuint Square, VAOB;
-	glGenVertexArrays(1, &VAOB);
-	glGenBuffers(1, &Square);
-	glBindVertexArray(VAOB);
-	glBindBuffer(GL_ARRAY_BUFFER, Square);
-	glBufferData(GL_ARRAY_BUFFER, Points.size() * sizeof(GLfloat), Points.data(), GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0); //Changed to sizeof GLfloat because it makes more sense.
-	glEnableVertexAttribArray(0);
-
-	shader.ClearShader();
 	shader.CreateFromFiles("Shaders/Basic2DShader.vert", "Shaders/Basic2DShader.frag");
-	glEnable(GL_LINE_SMOOTH);
-
+	
+	glEnable(GL_LINE_SMOOTH); //makes the line equally thick.
 	glLineWidth(10.0f);
-
-	//glEnable(GL_POINTS);
-	glPointSize(10.0f);
 	
 	while (!window.GetWindowShouldClose())
 	{
@@ -145,19 +98,15 @@ int main()
 		{
 			Remake(t,LineBuffer);
 		}
-		PlayerInput();
-		glfwPollEvents();
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glClearColor(0.01f, 0.1f, 0.02f, 1.0f);
+		PlayerInput(); //get player input
+		glfwPollEvents(); //update player input
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //clear the frame buffer
+		glClearColor(0.01f, 0.1f, 0.02f, 1.0f); //set backgroun colour
 
-		shader.UseShader();
-		glBindVertexArray(VAO);
-		
-		//glDrawArrays(GL_POINTS, 0, 350);
-		glDrawArrays(GL_LINES, 0, NumberOfPoints * 3);
+		shader.UseShader(); //use this shader
+		glBindVertexArray(VAO); //bind object to draw (Lines in this case)
+		glDrawArrays(GL_LINE_STRIP, 0, NumberOfPoints);
 
-		glBindVertexArray(VAOB);
-		glDrawArrays(GL_LINE_LOOP, 0, 4);
 
 		window.SwapBuffers();
 		
@@ -170,18 +119,13 @@ void Remake(float t,GLuint BufferIn)
 {
 	if (RemakeBuffer)
 	{
-		Points.clear();
-		for (size_t i = 0; i < 10; i++)
+		Points.clear(); //clear the vector
+		for (size_t i = 0; i < 100; i++) //100 points along line
 		{
 			Points.push_back(BezForShader(t * i, Posa.x, Posb.x, Posc.x, Posd.x));
 			Points.push_back(BezForShader(t * i, Posa.y, Posb.y, Posc.y, Posd.y));
-			Points.push_back(0.0f);
-
-			Points.push_back(BezForShader(t * i, Posa.x, Posb.x, Posc.x, Posd.x));
-			Points.push_back(BezForShader(t * i, Posa.y, Posb.y, Posc.y, Posd.y));
-			Points.push_back(0.0f);
 		}
-		RemakeBuffer = false;
+		RemakeBuffer = false; //reset remake buffer
 		glBindBuffer(GL_ARRAY_BUFFER, BufferIn);
 		glBufferData(GL_ARRAY_BUFFER, Points.size() * sizeof(GLfloat), Points.data(), GL_DYNAMIC_DRAW);
 	}
@@ -200,6 +144,30 @@ float BezForShader(float t, float wa, float wb, float wc, float wd)
 
 void SplineOneAttempt()
 {
+	//Polygon for texture (for compute shader)
+	GLfloat Verts[] =
+	{
+		-1.0f,-1.0f,0.0f,	0.0f,0.0f, //Botom Left
+		1.0f,-1.0f,0.0f,	1.0f,0.0f, //Botom right
+		-1.0f,1.0f,0.0f,	0.0f,1.0f, //Top Left
+		1.0f,1.0f,0.0f,		1.0f,1.0f, //Top Left
+
+	};
+
+	//Indices for polygon (for compute shader)
+	GLuint Inds[] =
+	{
+		0,1,2,
+		2,1,3
+	};
+
+	mesh.CreateMesh(Verts, Inds, 20, 6); //create polygon
+
+	shader.CreateFromFiles("Shaders/Shader.vert", "Shaders/Shader.frag");
+
+	Texture.CreateTexture(TexWidth, TexHeight);
+	Texture.UseTexture();
+
 	compShader.CreateComputeShaderFromFile("Shaders/Spline1.gl");
 	compShader.UseShader();
 	compShader.SetUniform3F("PosA", Posa.x,Posa.y, 0.0f);
@@ -234,6 +202,30 @@ void SplineOneAttempt()
 
 void SplineTwoAttempt()
 {
+	//Polygon for texture (for compute shader)
+	GLfloat Verts[] =
+	{
+		-1.0f,-1.0f,0.0f,	0.0f,0.0f, //Botom Left
+		1.0f,-1.0f,0.0f,	1.0f,0.0f, //Botom right
+		-1.0f,1.0f,0.0f,	0.0f,1.0f, //Top Left
+		1.0f,1.0f,0.0f,		1.0f,1.0f, //Top Left
+
+	};
+
+	//Indices for polygon (for compute shader)
+	GLuint Inds[] =
+	{
+		0,1,2,
+		2,1,3
+	};
+
+	mesh.CreateMesh(Verts, Inds, 20, 6); //create polygon
+
+	shader.CreateFromFiles("Shaders/Shader.vert", "Shaders/Shader.frag");
+
+	Texture.CreateTexture(TexWidth, TexHeight);
+	Texture.UseTexture();
+
 	compShader.CreateComputeShaderFromFile("Shaders/Spline2.gl");
 	compShader.UseShader();
 	compShader.SetUniform3F("PosA", Posa.x,Posa.y, 0.0f);
